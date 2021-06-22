f606d816bda830107e1072258e73dd14965d143a
Alex <alex+git@fremantle.io>
2021-06-21 19:32:47 -0700

    Release v0.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stylish.MixProject do
   def project do
     [
       app: :stylish,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.10",
       package: package(),
       start_permanent: Mix.env() == :prod,
